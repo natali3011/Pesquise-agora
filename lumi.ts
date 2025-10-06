d3ef1534d84a03a5835b5eb75a3d6672b8285bd1
natali3011 <104580279+natali3011@users.noreply.github.com>
2025-10-06 18:00:36 -0300

    Create lumi.ts

---
+++ lumi.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from '@lumi.new/sdk'
+
+export const lumi = createClient({
+  projectId: 'p365963720478613504',
+  apiBaseUrl: 'https://api.lumi.new',
+  authOrigin: 'https://auth.lumi.new',
+})
+
